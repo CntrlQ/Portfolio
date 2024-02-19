1a106f5fdb5b9dc6e76cecb015ac83eacb7a83e9
Nihal anil <43931373+CntrlX@users.noreply.github.com>
2024-02-19 22:49:42 +0530

    code Upload

---
+++ src/data/education.ts
@@ -0,0 +1,37 @@
+import { TShowCaseListDetatils } from "@/components/ShowCaseList";
+
+export const EDUCATION: TShowCaseListDetatils[] = [
+  {
+    title: "Bachelor Of Engineering in Computer Science",
+    organisation: {
+      name: "Universal Engineering College ",
+      href: "https://uec.ac.in",
+    },
+    date: "2020-2024",
+    location: "Thrissur, Kerala",
+    description:
+      "Relevant courses included Data Structures and Algorithms, Web Design, Cloud Computing, Artificial Neural Network and Database Management.",
+  },
+  {
+    title: "Higher Secondary School",
+    organisation: {
+      name: "G.V.H.S.S VALAPAD",
+      href: "",
+    },
+    date: "2018-2020",
+    location: "Thrissur, Kerala",
+    description:
+      "Completed the course in Electronics Bifocal Science with 73%. Started freelancing making 3D assets for games. Got interested in game development and learned unity and c#",
+  },
+  {
+    title: "Secondary School Education",
+    organisation: {
+      name: "G.V.H.S VALAPAD",
+      href: "",
+    },
+    date: "2017-2018",
+    location: "Thrissur, Kerala",
+    description:
+      "Learned good values,dedication and disciplne.",
+  },
+];
